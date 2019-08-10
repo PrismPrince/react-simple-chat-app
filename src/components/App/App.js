4796bd2b24e86813cd27032f2caff67a38fca8b6
markgaudiano <gaudianomark1@gmail.com>
2019-08-10 15:31:44 +0800

    add name filter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Names from './names'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
+    <div className="App">   
+      <Router>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Link
+            className="App-link"
+            to="/"
+          >
+            HOME
+          </Link>
+          <Link
+            className="App-link"
+            to="/names"
+          >
+            NAME
+          </Link>
+        </header>    
+        <Switch>
+          <Route path="/" exact component={Home}></Route>       
+          <Route path="/" component={Names}></Route>      
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+function Home() {
+  return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Home Page</h1>
     </div>
   );
 }
---
+++ src/components/App/names.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+import blank from './image/blank.png'
+
+class Names extends React.Component{
+  state = { searchString: '' }
+  handleChange = (e) => {
+   this.setState({ searchString:e.target.value });
+  }
+  render(){
+    var names =  [
+      { name: 'Jane Doe'},
+      { name: 'Mark Gaudiano'},
+      { name: 'Jan Karlo'},
+      { name: 'Dave Pacilan'},
+      { name: 'Geman Frutas'},
+
+    ],
+    searchString = this.state.searchString.trim().toLowerCase();
+    if (searchString.length > 0) {
+       names = names.filter(function(i) {
+         return i.name.toLowerCase().match( searchString);
+       });
+    }
+
+    return( 
+      <div className="App">
+        <div className="container py-5">
+          <div className="plist pb-5">
+
+            <button type="button" className="btn btn-primary float-left">BACK</button>
+      
+            <form className="pb-5">
+              <input type="text" placeholder="Search" className="float-right p-2" value={this.state.searchString} onChange={this.handleChange} />
+              
+            </form>
+
+            <div className="allnames pt-5">
+              {names.map(list => (
+                <div className="names align-self-center" key={list.name}>
+                  <a href="/" className="btn ">
+                    <img src={blank} alt="blank" className="float-left"
+                    style={{
+                      width: "40px", height: "40px"
+                    }}
+                     />
+                    {list.name}
+                  </a> 
+                  
+                </div>
+              ))}
+            </div>
+          </div>    
+        </div>
+      </div>
+
+      
+    )
+  }
+}
+export default Names;
+  
